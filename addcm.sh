0ee0c520d1d6a47f59cc43d47c6e80e299b3cf25
たなかともあき <github@bamboo-copter.com>
2012-09-26 10:48:11 +0900

    configure Makefile 作るシェルスクリプトかっこよくしたよ

--- addcm.sh
+++ addcm.sh
@@ -9,46 +9,79 @@ echo "cd $1"
 cd $1
 pwd
 
-echo "make clean"
+function runproc()
+{
+    echo -n "$2."
+
+    while :
+    do
+	if [ `ps -p $1 |grep -v PID |wc -l |awk '{print $1}'` -eq 0 ]; then
+	    break
+	fi
+	echo -n "."
+	sleep 1
+    done
+
+    echo " ok"
+}
+
 make clean >/dev/null 2>&1 &
-while :
-do
-    if [ `ps ax |grep make |grep -v grep |wc -l |awk '{print $1}'` -eq 0 ]; then
-	break
-    fi
-    echo -n "."
-    sleep 1
-done
+runproc $! "make clean"
 
-echo "done"
+rm -f configure.in &
+runproc $! "delete configure.in"
 
-echo "delete "
-rm -f configure.in
-rm -f Makefile.am
+rm -f Makefile.am &
+runproc $! "delete Makefile.am"
+
 rm -f src/Makefile.am
+runproc $! "delete src/Makefile.am"
 
-echo "touch new file"
-touch INSTALL NEWS README COPYING AUTHORS ChangeLog
+touch INSTALL NEWS README COPYING AUTHORS ChangeLog &
+runproc $! "touch INSTALL NEWS README COPYING AUTHORS ChangeLog"
 
-echo "autoscan"
-autoscan
+autoscan &
+runproc $! "run autoscan"
 
-mv configure.scan configure.in
+mv configure.scan configure.in &
+runproc $! "rename configure.scan => configure.in"
 
-echo "AM_INIT_AUTOMAKE([FULL-PACKAGE-NAME], [VERSION])" >>configure.in
-[ ! -z $4 ] && echo "AC_PROG_LEX" >>configure.in
-#[ ! -z $5 ] && echo "AM_PROG_YACC" >>configure.in
+echo "AM_INIT_AUTOMAKE([FULL-PACKAGE-NAME], [VERSION])" >>configure.in &
+runproc $! "add automake directive"
 
-gsed -i "s|FULL-PACKAGE-NAME|$prog|g" configure.in
-gsed -i "s|VERSION|$ver|g" configure.in
-gsed -i "s|BUG-REPORT-ADDRESS|$addr|" configure.in
+echo "AC_PROG_LEX" >>configure.in &
+runproc $! "add flex directive"
 
-echo "SUBDIRS = src" >Makefile.am
+echo "AC_PROG_YACC" >>configure.in &
+runproc $! "add bison directive"
 
-echo "bin_PROGRAMS = $prog" >src/Makefile.am
-echo "${prog}_SOURCES = $src" >>src/Makefile.am
+gsed -i "s|FULL-PACKAGE-NAME|$prog|g" configure.in &
+runproc $! "change package name"
 
-aclocal
-autoconf
-autoheader
-automake --copy --add-missing
+gsed -i "s|VERSION|$ver|g" configure.in &
+runproc $! "change version"
+
+gsed -i "s|BUG-REPORT-ADDRESS|$addr|" configure.in &
+runproc $! "change bugreport address"
+
+echo "SUBDIRS = src" >Makefile.am &
+runproc $! "add source directory directive"
+
+
+echo "bin_PROGRAMS = $prog" >src/Makefile.am &
+runproc $! "add program name directive"
+
+echo "${prog}_SOURCES = $src" >>src/Makefile.am &
+runproc $! "add source file list"
+
+aclocal &
+runproc $! "aclocal"
+
+autoconf &
+runproc $! "autoconf"
+
+autoheader &
+runproc $! "autoheader"
+
+automake --copy --add-missing &
+runproc $! "automake"
